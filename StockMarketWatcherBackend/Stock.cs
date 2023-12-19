73047fa7fa5a5e078fdf991e41e481282a5e7b1b
srose0040 <115491189+srose0040@users.noreply.github.com>
2023-12-19 16:34:52 -0500

    Created boilerplate model for Stocks

---
+++ StockMarketWatcherBackend/Stock.cs
@@ -0,0 +1,18 @@
+﻿namespace StockMarketWatcherBackend
+{
+    public class Stock
+    {
+        public string Name { get; set; }
+        public float PriceNow { get; set; }
+        public float StartingPrice { get; set; }
+        public List <float> Diff { get; set; }
+
+        public Stock(string name, float startingPrice, float priceNow, List<float> diff)
+        {
+            Name = name;
+            PriceNow = priceNow;
+            StartingPrice = startingPrice;
+            Diff = diff;
+        }
+    }
+}
