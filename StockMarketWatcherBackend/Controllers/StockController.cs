5826068a984917d4c556599e2a26a1a38b4fce15
srose0040 <115491189+srose0040@users.noreply.github.com>
2023-12-19 16:38:09 -0500

    Created api conroller class file for stock

---
+++ StockMarketWatcherBackend/Controllers/StockController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StockMarketWatcherBackend.Controllers
+{
+    [Route("[controller]")] // Removed "api/" so the route is just /stock
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+    }
+}
